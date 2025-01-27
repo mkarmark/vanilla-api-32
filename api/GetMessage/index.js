648cbc1e78a6e008f9b280c5ccd12935e9768bc1
mkarmark <mikarmar@microsoft.com>
2025-01-27 10:58:36 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
   context.res = {
     headers: {
-      'Set-Cookie': 'foo=bar; Secure=false'
+      'Set-Cookie': 'foo=bar; Secure=true'
     },
     // status: 200, /* Defaults to 200 */
     body: { text: "Hello from the API" },
